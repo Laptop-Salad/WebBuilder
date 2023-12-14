e46f15d4c70d6b59c4dccdd15b09444a440b26ac
Amanda Wallis <awallis1609@gmail.com>
2023-12-14 11:41:57 +0000

    add paragraph

--- elemCreator.js
+++ elemCreator.js
@@ -1,4 +1,4 @@
-import { Heading } from "./elementsClasses.js";
+import { Heading, Paragraph } from "./elementsClasses.js";
 
 const headings = ["h1", "h2", "h3", "h4", "h5", "h6"];
 const headingsConfig = {
@@ -46,6 +46,24 @@ function createHeading (elemName, pageTarget) {
     return new Heading(elem, pageTarget);
 }
 
+
+function createParagraph (pageTarget) {
+    /**
+     * Initialises a paragraph element and creates its element class
+     * @param {String} pageTarget Which page the element will be placed
+     * @return {Paragraph}
+     */
+    let elem;
+    elem = document.createElement("p");
+    elem.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla aliquet ipsum in orci vulputate, nec consequat libero maximus. Curabitur hendrerit et nulla eu maximus. Pellentesque eget erat id est tempus aliquet. Donec suscipit urna sit amet euismod pulvinar. Etiam imperdiet odio vel mi tincidunt, a interdum est faucibus. Fusce molestie venenatis sem et tincidunt. Sed congue tempor lectus, vel auctor massa. Nam dictum tempus magna, eu lobortis velit aliquet nec. Praesent egestas tellus velit. Mauris placerat rhoncus lectus. Duis id iaculis nisl. Nulla eu vehicula enim, sit amet cursus nunc."
+    elem.style.position = "absolute";
+    elem.style.color = "black";
+    elem.style.left = 0;
+    elem.style.top = 0;
+    elem.style.fontSize = "16px";
+    return new Paragraph(elem, pageTarget);
+}
+
 export function mapElemCreator (elemName, pageTarget) {
     /**
      * Finds the matching element initialiser
@@ -55,5 +73,7 @@ export function mapElemCreator (elemName, pageTarget) {
      */
     if (headings.includes(elemName)) {
         return createHeading(elemName, pageTarget);
+    } else if (elemName == "p") {
+        return createParagraph(pageTarget);
     }
 }
--- elementsClasses.js
+++ elementsClasses.js
@@ -79,3 +79,78 @@ export class Heading {
         this.page = value;
     }
 } 
+
+export class Paragraph {
+    /**
+     * Handles paragraph
+     */
+    constructor(elem, page) {        
+        this.elem = elem;
+        this.page = page;
+    }
+
+    updateMapper(key, value) {
+        switch (key) {
+            case "fontSize":
+                setFontSize(this.elem, value);
+                break;
+            case "color":
+                setColor(this.elem, value);
+                break;
+            case "text":
+                setText(this.elem, value);
+                break;
+            case "x":
+                setX(this.elem, value);
+                break;
+            case "y":
+                setY(this.elem, value);
+                break;
+            case "position":
+                setPosition(this.elem, value);
+                break;
+            case "page":
+                this.page = value;
+                break;
+        }
+    }
+
+    getEditableProperties() {
+        return [
+            {
+                "system_name" : "fontSize",
+                "display_name": "Text Size (px)",
+                "value": this.elem.style.fontSize
+            }, 
+            {
+                "system_name": "color",
+                "display_name": "Colour",
+                "value": this.elem.style.color
+            },
+            {
+                "system_name": "text",
+                "display_name": "Text",
+                "value": this.elem.innerHTML
+            },
+            {
+                "system_name": "x",
+                "display_name": "x",
+                "value": this.elem.style.left
+            },
+            {
+                "system_name": "y",
+                "display_name": "y",
+                "value": this.elem.style.top
+            },
+            {
+                "system_name": "position",
+                "display_name": "Position",
+                "value": this.elem.style.position
+            },
+        ]
+    }
+
+    set setPage(value) {
+        this.page = value;
+    }
+} 
